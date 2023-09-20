70c0b5bb12bfdeaaacfcf59fd244e45bb1f26f22
freeagentworks <freeagentworks@gmail.com>
2023-09-20 23:44:55 +0900

    2nd commit

--- mpfChart.py
+++ mpfChart.py
@@ -13,9 +13,10 @@ col1, col2 = st.columns(2)
 with col1:
     ticker = st.text_input("ティッカーコード:",'^N225')
 with col2:
-    dsp_days = st.text_input("表示日数", 150)
-    #submit_btn = st.button("送信")
-
+    st.write('&nbsp;')
+    show_dataset = st.checkbox('データ表示', False)
+    
+dsp_days = st.slider("表示日数（日）:",min_value=30,max_value=1000,value=100)   
 st.markdown("---")
 
 
@@ -25,24 +26,25 @@ st.sidebar.caption("設定項目を入力してください")
 with st.sidebar.form("settings_form"):
     show_volume = st.checkbox('出来高表示',False)
     
+    
     chart_types = [
         'candle', 'ohlc', 'line', 'renko', 'pnf'
     ]
     chart_type = st.selectbox('チャートタイプ', options=chart_types, index=chart_types.index('candle'))
 
-    chart_styles = [
-        'default', 'binance', 'blueskies', 'brasil', 
-        'charles', 'checkers', 'classic', 'yahoo',
-        'mike', 'nightclouds', 'sas', 'starsandstripes'
-    ]
+    chart_styles = mpf.available_styles()
     chart_style = st.selectbox('チャートスタイル', options=chart_styles, index=chart_styles.index('yahoo'))
 
-    marketcolors = [
-        'default', 'binance', 'blueskies', 'brasil', 
-        'charles', 'checkers', 'classic', 'yahoo',
-        'mike', 'nightclouds', 'sas', 'starsandstripes'        
-    ]
+    # marketcolors = [
+    #     'default', 'binance', 'blueskies', 'brasil', 
+    #     'charles', 'checkers', 'classic', 'yahoo',
+    #     'mike', 'nightclouds', 'sas', 'starsandstripes'        
+    # ]
+    
+    marketcolors = mpf.available_styles()
     market_color = st.selectbox('マーケットカラー', options=marketcolors, index=chart_styles.index('yahoo')) 
+    mc = mpf.make_marketcolors(base_mpf_style=market_color)
+    s  = mpf.make_mpf_style(base_mpf_style=chart_style,marketcolors=mc)
     
     st.text("移動平均線")
     sma1 = int(st.text_input("短期", 5))
@@ -59,7 +61,7 @@ def show_chart(ticker,df):
    fig, ax = mpf.plot(
        df,
         title=ticker,
-        style=chart_style,
+        style=s,
         type=chart_type,
         figsize=(20,10),
         volume=show_volume,
@@ -71,7 +73,7 @@ def show_chart(ticker,df):
 if ticker:
     df = yf.download(ticker).tail(int(dsp_days))
     show_chart(ticker,df)
-
-
-#st.dataframe(df)
+    if show_dataset:
+        st.markdown("---")       
+        st.dataframe(df)
     
